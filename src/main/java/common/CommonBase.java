2df1632b883fa42fe644de3bc6917488ee602a00
VanVK <vanvk@phugroup.com>
2023-09-05 17:58:13 +0700

    update day 18

--- src/main/java/common/CommonBase.java
+++ src/main/java/common/CommonBase.java
@@ -1,4 +1,5 @@
 package automation.common;
+
 import java.io.File;
 import java.util.concurrent.TimeUnit;
 
@@ -24,114 +25,125 @@ import org.testng.Assert;
 import static automation.common.TestLogger.*;
 
 public class CommonBase {
-	public WebDriver driver;
-	protected String baseUrl = "https://staging.capa.ai/";
-	protected int DEFAULT_TIMEOUT = 20000;
-	protected int WAIT_INTERVAL = 1000;
-	public int loopCount = 0;
-	public final int ACTION_REPEAT = 5;
-	public Actions action;
+    public WebDriver driver;
+    protected String baseUrl = "https://staging.capa.ai/";
+    protected int DEFAULT_TIMEOUT = 20000;
+    protected int WAIT_INTERVAL = 1000;
+    public int loopCount = 0;
+    public final int ACTION_REPEAT = 5;
+    public Actions action;
 
-	/**
-	 * init Driver
-	 * 
-	 * @param URL
-	 */
-	public WebDriver initDriverTest(String... URL) {
-		String url1 = URL.length > 0 ? URL[0] : baseUrl;
-		String proName = URL.length > 2 ? URL[2] : "";
-		String broName = URL.length > 1 ? URL[1] : "chrome";
-		String url2 = System.getProperty("Url");
-		String browser = System.getProperty("browser");
-		String plaForm = System.getProperty("platForm");
-		WebDriver dr = null;
+    /**
+     * init Driver
+     *
+     * @param URL
+     */
+    public WebDriver initDriverTest(String... URL) {
+        String url1 = URL.length > 0 ? URL[0] : baseUrl;
+        String proName = URL.length > 2 ? URL[2] : "";
+        String broName = URL.length > 1 ? URL[1] : "chrome";
+        String url2 = System.getProperty("Url");
+        String browser = System.getProperty("browser");
+        String plaForm = System.getProperty("platForm");
+        WebDriver dr = null;
 
-		if (browser == null) {
-			browser = broName;
-		}
-		ChromeOptions options = new ChromeOptions();
-		if ("chrome".equals(browser)) {
-			System.setProperty("webdriver.chrome.driver",
-			System.getProperty("user.dir") + "\\driver\\chromedriver.exe");		
-			dr = new ChromeDriver();
-		} else if ("iexplorer".equals(browser)) {
-			dr = new InternetExplorerDriver();
-		} else if ("safari".equals(browser)) {
-			dr = new SafariDriver();
-		} else {
-			if (proName != null && proName != "") {
-				FirefoxProfile ffprofile = new FirefoxProfile(new File(proName));
-				dr = new FirefoxDriver(ffprofile);
-			} else {
-				FirefoxProfile fxProfile = new FirefoxProfile();
-				File browserAppPath = new File("/usr/bin/geckodriver");
-				FirefoxBinary ffBin = new FirefoxBinary(browserAppPath);
-				dr = new FirefoxDriver(ffBin, fxProfile);
-			}
-		}
-		if (url2 != "" && url2 != null) {
-			dr.get(url2);
-		} else {
-			dr.get(url1);
-		}
-		dr.manage().window().maximize();
-		dr.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
-		return dr;
-	}
+        if (browser == null) {
+            browser = broName;
+        }
+        ChromeOptions options = new ChromeOptions();
+        if ("chrome".equals(browser)) {
+            System.setProperty("webdriver.chrome.driver",
+                    System.getProperty("user.dir") + "\\driver\\chromedriver.exe");
+            dr = new ChromeDriver();
+        } else if ("iexplorer".equals(browser)) {
+            dr = new InternetExplorerDriver();
+        } else if ("safari".equals(browser)) {
+            dr = new SafariDriver();
+        } else {
+            if (proName != null && proName != "") {
+                FirefoxProfile ffprofile = new FirefoxProfile(new File(proName));
+                dr = new FirefoxDriver(ffprofile);
+            } else {
+                FirefoxProfile fxProfile = new FirefoxProfile();
+                File browserAppPath = new File("/usr/bin/geckodriver");
+                FirefoxBinary ffBin = new FirefoxBinary(browserAppPath);
+                dr = new FirefoxDriver(ffBin, fxProfile);
+            }
+        }
+        if (url2 != "" && url2 != null) {
+            dr.get(url2);
+        } else {
+            dr.get(url1);
+        }
+        dr.manage().window().maximize();
+        dr.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+        return dr;
+    }
 
-	public WebDriver initChromeDriver(String URL)
-	{
-		ChromeOptions options = new ChromeOptions();
-		System.setProperty("webdriver.chrome.driver",
-		System.getProperty("user.dir") + "\\driver\\chromedriver.exe");		
-		driver = new ChromeDriver(options);
-		driver.manage().window().maximize();
-		driver.get(URL);
-		driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
-		return driver;
-	}
-	
-	/**
-	 * pause driver in timeInMillis
-	 * 
-	 * @param timeInMillis
-	 */
-	public void pause(long timeInMillis) {
-		try {
-			Thread.sleep(timeInMillis);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
+    public WebDriver initChromeDriver(String URL) {
+        System.out.println("Launching Chrome browser...");
+        ChromeOptions options = new ChromeOptions();
+        System.setProperty("webdriver.chrome.driver",
+                System.getProperty("user.dir") + "\\driver\\chromedriver.exe");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.get(URL);
+        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+        return driver;
+    }
+    public WebDriver initFirefoxDriver() {
+        System.out.println("Launching Firefox browser...");
+        System.setProperty("webdriver.gecko.driver",
+                System.getProperty("user.dir") + "\\driver\\chromedriver.exe");
+        driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        return driver;
+    }
 
-	/**
-	 * click on an element */
-	
-	public void click(Object locator) {
-		By xPath = locator instanceof By ? (By) locator : By.xpath(locator.toString());
-		WebDriverWait wait = new WebDriverWait(driver, 10);
-		WebElement elementClick = wait.until(ExpectedConditions
-				.elementToBeClickable(xPath));
-		elementClick.click();
-	}
-	
-	/** get absolute path of file
-	 * 
-	 * @param relativeFilePath
-	 * @return
-	 */
-	public String getAbsoluteFilePath(String relativeFilePath) {
-		String curDir = System.getProperty("user.dir");
-		String absolutePath = curDir + relativeFilePath;
-		return absolutePath;
-	}
 
-	
-	/**
-	 * 
-	 * @param locator
-	 * @param opParams
-	 */
+    /**
+     * pause driver in timeInMillis
+     *
+     * @param timeInMillis
+     */
+    public void pause(long timeInMillis) {
+        try {
+            Thread.sleep(timeInMillis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * click on an element
+     */
+
+    public void click(Object locator) {
+        By xPath = locator instanceof By ? (By) locator : By.xpath(locator.toString());
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        WebElement elementClick = wait.until(ExpectedConditions
+                .elementToBeClickable(xPath));
+        elementClick.click();
+    }
+
+    /**
+     * get absolute path of file
+     *
+     * @param relativeFilePath
+     * @return
+     */
+    public String getAbsoluteFilePath(String relativeFilePath) {
+        String curDir = System.getProperty("user.dir");
+        String absolutePath = curDir + relativeFilePath;
+        return absolutePath;
+    }
+
+
+    /**
+     *
+     * @param locator
+     * @param opParams
+     */
 //	public void mouseOverAndClick(Object locator, Object... opParams) {
 //		WebElement element;
 //		int notDisplay = (Integer) (opParams.length > 0 ? opParams[0] : 0);
@@ -144,26 +156,26 @@ public class CommonBase {
 //		}
 //	}
 
-	/**
-	 * quit driver if driver existed
-	 * 
-	 * @param dr
-	 */
-	public void quitDriver(WebDriver dr) {
-		if (dr.toString().contains("null")) {
-			System.out.print("All Browser windows are closed ");
-		} else {
-			driver.manage().deleteAllCookies();
-			dr.close();
-		}
-	}
+    /**
+     * quit driver if driver existed
+     *
+     * @param dr
+     */
+    public void quitDriver(WebDriver dr) {
+        if (dr.toString().contains("null")) {
+            System.out.print("All Browser windows are closed ");
+        } else {
+            driver.manage().deleteAllCookies();
+            dr.close();
+        }
+    }
 
-	/**
-	 * switch to a frame
-	 * 
-	 * @param locator
-	 * @param opParams
-	 */
+    /**
+     * switch to a frame
+     *
+     * @param locator
+     * @param opParams
+     */
 //	public void switchToFrame(Object locator, Object... opParams) {
 //		info("Switch to frame " + locator);
 //		int notDisplay = (Integer) (opParams.length > 0 ? opParams[0] : 0);
@@ -174,90 +186,89 @@ public class CommonBase {
 //		}
 //	}
 
-	/**
-	 * back to main frame
-	 */
-	public void switchToParentFrame() {
-		try {
-			driver.switchTo().defaultContent();
-		} catch (Exception e) {
-			switchToParentFrame();
-		}
-	}
+    /**
+     * back to main frame
+     */
+    public void switchToParentFrame() {
+        try {
+            driver.switchTo().defaultContent();
+        } catch (Exception e) {
+            switchToParentFrame();
+        }
+    }
 
-	/**
-	 * accept unexpected alert
-	 */
-	public void acceptAlert() {
-		try {
-			Alert alert = driver.switchTo().alert();
-			alert.accept();
+    /**
+     * accept unexpected alert
+     */
+    public void acceptAlert() {
+        try {
+            Alert alert = driver.switchTo().alert();
+            alert.accept();
 
-		} catch (NoAlertPresentException ex) {
-			info("No Alert present");
-			;
-		}
-	}
+        } catch (NoAlertPresentException ex) {
+            info("No Alert present");
+            ;
+        }
+    }
 
-	/**
-	 * 
-	 * @param locator
-	 * @param opParams
-	 */
+    /**
+     *
+     * @param locator
+     * @param opParams
+     */
 //	public void scrollToElement(Object locator, Object... opParams) {
 //		int notDisplay = (Integer) (opParams.length > 0 ? opParams[0] : 0);
 //		WebElement element = getElementPresent(locator, DEFAULT_TIMEOUT, 1, notDisplay);
 //		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
 //	}
 
-	/**
-	 * check field is null = ""
-	 * 
-	 * @param s
-	 */
-	public void verifyNull(String s) {
-		if (!s.equalsIgnoreCase("")) {
-			Assert.fail("Du lieu khong null");
-		}
-	}
+    /**
+     * check field is null = ""
+     *
+     * @param s
+     */
+    public void verifyNull(String s) {
+        if (!s.equalsIgnoreCase("")) {
+            Assert.fail("Du lieu khong null");
+        }
+    }
 
-	/**
-	 * tra ve so lan xuat hien cua 1 xau trong chuoi
-	 * 
-	 * @param value
-	 * @param array
-	 * @return
-	 */
-	public String checkValueInArray(String value, String[] array) {
-		int soLan = 0;
-		if (value != null && value != "" && array.length > 0) {
-			for (int i = 0; i < array.length; i++) {
-				if (value.equalsIgnoreCase(array[i])) {
-					soLan++;
-					info("Chuoi \"" + value + "\" xuat hien trong mang lan thu: " + soLan);
-				}
-			}
-			return String.valueOf(soLan);
-		} else {
-			info("Chuoi can kiem tra dang null hoac mang khong co phan tu");
-			return "";
-		}
-	}
+    /**
+     * tra ve so lan xuat hien cua 1 xau trong chuoi
+     *
+     * @param value
+     * @param array
+     * @return
+     */
+    public String checkValueInArray(String value, String[] array) {
+        int soLan = 0;
+        if (value != null && value != "" && array.length > 0) {
+            for (int i = 0; i < array.length; i++) {
+                if (value.equalsIgnoreCase(array[i])) {
+                    soLan++;
+                    info("Chuoi \"" + value + "\" xuat hien trong mang lan thu: " + soLan);
+                }
+            }
+            return String.valueOf(soLan);
+        } else {
+            info("Chuoi can kiem tra dang null hoac mang khong co phan tu");
+            return "";
+        }
+    }
 
-	/**
-	 * 
-	 * @param string
-	 * @param split
-	 * @return
-	 */
-	public String[] convertStringToArray(String string, String split) {
-		String[] a = new String[100];
-		if (string != null && string != "") {
-			a = string.split(split);
-		} else {
-			info("Xau ky tu can chuyen sang mang ");
-		}
-		return a;
-	}
+    /**
+     * @param string
+     * @param split
+     * @return
+     */
+    public String[] convertStringToArray(String string, String split) {
+        String[] a = new String[100];
+        if (string != null && string != "") {
+            a = string.split(split);
+        } else {
+            info("Xau ky tu can chuyen sang mang ");
+        }
+        return a;
+    }
 
 }
